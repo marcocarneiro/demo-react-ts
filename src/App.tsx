bd70987dba806981644abd20271eb915225dfa70
marcocarneiro <marco.carneiro7@gmail.com>
2024-04-10 16:52:29 -0300

    Implementação de Temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes'
+import { ThemeProvider } from '@mui/material'
+import { LightTheme } from './themes'
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme} >
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>    
   )
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Button } from '@mui/material'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 export const AppRoutes = () => {
-	return(
-        <Routes>
-            <Route path="/" element={<p>Página Inicial</p>}  />
-            <Route path="*" element={<p> 404 - página não encontrada </p>}  />
-            {/* Exemplo de redirecionamento:
-            <Route path="*" element={<Navigate to="/pagina-inicial" />}  /> 
-            */}
-        </Routes>
-    )
+
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>Teste</Button>} />
+
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  )
 }
