999e1e2b0f1c18a530c4e3749a595ea1bcf05970
marcocarneiro <marco.carneiro7@gmail.com>
2024-04-10 21:30:17 -0300

    Ajustes na organização do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes'
 import { ThemeProvider } from '@mui/material'
-import { LightTheme } from './themes'
+import { LightTheme } from './shared/themes'
 
 function App() {
   return (
